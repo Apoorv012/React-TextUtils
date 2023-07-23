22b6510a4fc2ebc611489949eb98e99ecd06e694
Apoorv Mittal <apoorvmitthu@gmail.com>
2023-07-23 20:59:12 +0530

    Preview and lowercase button

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,27 +6,53 @@ export default function TextForm(props) {
     setText(newText);
   };
 
-  const handleOnChanged = (event) => {
-    setText(event.target.value);
+  const handleLowercaseClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
   };
 
-  const [text, setText] = useState("Enter text here");
+  const handleOnChanged = (event) => {
+    setText(event.target.value);
+    setNumOfWords(text === "" ? 0 : text.split(" ").length);
+    setTimeToRead(Math.ceil(numOfWords * 0.008));
+  };
+
+  const [text, setText] = useState("Enter text here!");
+  const [numOfWords, setNumOfWords] = useState(text.split(" ").length);
+  const [TimeToRead, setTimeToRead] = useState(Math.ceil(numOfWords * 0.008));
 
   return (
     <>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          id="myBox"
-          rows="8"
-          onChange={handleOnChanged}
-        ></textarea>
+      <div className="container my-3">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            id="myBox"
+            rows="8"
+            onChange={handleOnChanged}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUppercaseClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLowercaseClick}>
+          Convert to Lowercase
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUppercaseClick}>
-        Convert to Uppercase
-      </button>
+      <div className="container my-3">
+        <h3>Text Summary</h3>
+        <p>
+          {numOfWords} {numOfWords <= 1 ? "word" : "words"} and {text.length}{" "}
+          characters.
+        </p>
+        <p>
+          {TimeToRead} {TimeToRead <= 1 ? "minute" : "minutes"} read
+        </p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
     </>
   );
 }
